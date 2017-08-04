e884cb9c78da88e316cdef822a4cb0711b140d94
fogus <mefogus@gmail.com>
2017-08-04 15:08:04 -0400

    Latest CLJ and Spec

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject reinen-vernunft "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Code conversations in Clojure regarding the application of pure reasoning algorithms."
+  :url "https://github.com/fogus/reinen-vernunft"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [org.clojure/spec.alpha "0.1.123"]])
