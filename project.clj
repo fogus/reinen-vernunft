4d2899a887cab185c3762e923dcc9fac4b1c1830
fogus <mefogus@gmail.com>
2017-08-04 16:25:12 -0400

    Added latest version of core.unify.

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [org.clojure/spec.alpha "0.1.123"]
-                 [org.clojure/core.unify "0.5.6"]])
+                 [org.clojure/core.unify "0.5.7"]])
