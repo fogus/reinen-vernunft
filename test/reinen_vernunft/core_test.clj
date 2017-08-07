2b0b877792ea0e8c329ede3bb44fd798cb342103
fogus <mefogus@gmail.com>
2017-08-07 14:05:08 -0400

    Starting on unifications tests.

--- test/reinen_vernunft/core_test.clj
+++ test/reinen_vernunft/core_test.clj
@@ -2,6 +2,14 @@
   (:require [clojure.test :refer :all]
             [fogus.reinen-vernunft.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def rules
+  '[{:antecedent   [[?id   :emergency/type :emergency.type/fire]]
+     :consequent [[-1000 :response/type  :response.type/activate-sprinklers]
+                  [-1000 :response/to    ?id]]}
+    {:antecedent   [[?id   :emergency/type :emergency.type/flood]]
+     :consequent [[-1002 :response/type  :response.type/kill-electricity]
+                  [-1002 :response/to    ?id]]}])
+
+(def all-facts #{[-50 :emergency/type :emergency.type/fire]
+                 [-51 :emergency/type :emergency.type/flood]})
+
