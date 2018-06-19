a504c094e88b2dc2e296e31671d6637b6ca61843
fogus <mefogus@gmail.com>
2018-06-19 12:42:30 -0400

    Updating deps

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject fogus/reinen-vernunft "0.1.1-SNAPSHOT"
-  :description "Code conversations in Clojure regarding the application of pure reasoning algorithms."
+  :description "Code conversations in Clojure regarding the application of pure search, reasoning, and query algorithms."
   :url "https://github.com/fogus/reinen-vernunft"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/spec.alpha "0.1.123"]
+                 [org.clojure/spec.alpha "0.1.143"]
                  [org.clojure/core.unify "0.5.7"]]
-  :profiles {:dev {:dependencies [[datascript "0.16.4"]]}})
+  :profiles {:dev {:dependencies [[datascript "0.16.6"]]}})
