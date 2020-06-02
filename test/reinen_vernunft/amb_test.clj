7334f43c49b50e1adb6ef0a9be7e051863eec835
Fogus <mefogus@gmail.com>
2020-06-02 10:16:44 -0400

    Test cases for simple and complex bindings. Use of accept. Basic form of amb.

--- test/reinen_vernunft/amb_test.clj
+++ test/reinen_vernunft/amb_test.clj
@@ -2,3 +2,19 @@
   (:require [clojure.test :refer :all]
             [fogus.reinen-vernunft.amb :as rv]))
 
+(deftest test-amb-simple-binding
+  (is (= 3 (rv/amb [x (range 10)]
+                   (rv/accept (= x 3)
+                     x)))))
+
+(deftest test-amb-complex-binding
+  (is (= ["that" "thing" "grows" "slowly"]
+         (rv/amb [A ["the" "that" "a"]
+                  B ["frog" "elephant" "thing"]
+                  C ["walked" "treaded" "grows"]
+                  D ["slowly" "quickly"]]
+                 
+                 (rv/accept (and (= (last A) (first B))
+                                 (= (last B) (first C))
+                                 (= (last C) (first D)))
+                   [A B C D])))))
