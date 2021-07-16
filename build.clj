91ceced023c8c395e060a3ff93492e693b21b31d
Fogus <mefogus@gmail.com>
2021-07-16 15:11:12 -0400

    tweaks to build.clj

--- build.clj
+++ build.clj
@@ -4,13 +4,16 @@
 (def lib 'fogus/reinen-vernunft)
 (def description "Code conversations in Clojure regarding the application of pure search, reasoning, and query algorithms.")
 (def version (format "0.1.%s" (b/git-count-revs nil)))
-(def class-dir "target/classes")
-(def basis (b/create-basis {:project "deps.edn"}))
-(def jar-file (format "target/%s-%s.jar" (name lib) version))
+(def target-dir "target")
+(def class-dir (str target-dir "/" "classes"))
+(def jar-file (format "%s/%s-%s.jar" target-dir (name lib) version))
 (def src ["src/clj"])
+(def basis (b/create-basis {:project "deps.edn"}))
 
-(defn clean [_]
-  (b/delete {:path "target"}))
+(defn clean
+  "Delete the build target directory"
+  [_]
+  (b/delete {:path target-dir}))
 
 (defn jar [_]
   (b/write-pom {:class-dir class-dir
