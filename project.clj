36176f4d51d7915c2d438060e2d529a4d4a9fc02
Fogus <mefogus@gmail.com>
2021-07-20 08:46:56 -0400

    removed project.clj

--- project.clj
+++
@@ -1,9 +0,0 @@
-(defproject fogus/reinen-vernunft "0.1.1-SNAPSHOT"
-  :description "Code conversations in Clojure regarding the application of pure search, reasoning, and query algorithms."
-  :url "https://github.com/fogus/reinen-vernunft"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.11.0-alpha1"]
-                 [org.clojure/core.unify "0.5.7"]
-                 [evalive "1.1.0"]]
-  :profiles {:dev {:dependencies [[datascript "1.2.2"]]}})
